f45dfe3949d32ab6025ee6e2033e60a14cb29076
netoxdw <neto.guevara.tapia@gmail.com>
2024-04-02 16:18:46 -0600

    subiendo repor

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,68 +49,6 @@ window.addEventListener("scroll", function () {
 
 
 
-/**
- * SLIDER
- */
-
-const sliders = document.querySelectorAll("[data-slider]");
-
-const initSlider = function(currentSlider) {
-
-  const sldierContainer = currentSlider.querySelector("[data-slider-container]");
-  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
-  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
-
-  let currentSlidePos = 0;
-
-  const moveSliderItem = function () {
-    sldierContainer.style.transform = `translateX(-${sldierContainer.children[currentSlidePos].offsetLeft}px)`;
-  }
-
-  /**
-   * NEXT SLIDE
-   */
-
-  const slideNext = function () {
-    const slideEnd = currentSlidePos >= sldierContainer.childElementCount - 1;
-
-    if (slideEnd) {
-      currentSlidePos = 0;
-    } else {
-      currentSlidePos++;
-    }
-
-    moveSliderItem();
-  }
-
-  sliderNextBtn.addEventListener("click", slideNext);
-
-  /**
-   * PREVIOUS SLIDE
-   */
-
-   const slidePrev = function () {
-
-    if (currentSlidePos <= 0) {
-      currentSlidePos = sldierContainer.childElementCount - 1;
-    } else {
-      currentSlidePos--;
-    }
-
-    moveSliderItem();
-  }
-
-  sliderPrevBtn.addEventListener("click", slidePrev);
-
-  const dontHaveExtraItem = sldierContainer.childElementCount <= 1;
-  if (dontHaveExtraItem) {
-    sliderNextBtn.style.display = "none";
-    sliderPrevBtn.style.display = "none";
-  }
-
-}
-
-for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
 
 
 
